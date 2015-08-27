d6e0be9fb1fb5c09cf33d30c8e4e622865dedeaf
398058068@qq.com <398058068@qq.com>
2015-08-27 19:45:38 +0800

    Initial commit

---
+++ src/main/java/factory/simple/SendFactory.java
@@ -0,0 +1,18 @@
+package factory.simple;
+
+/**
+ * Created by An0nymous on 15/8/27.
+ */
+public class SendFactory {
+
+    public Sender produce(String type) {
+        if ("mail".equals(type)) {
+            return new MailSender();
+        } else if ("sms".equals(type)) {
+            return new SmsSender();
+        } else {
+            System.out.println("请输入正确的类型!");
+            return null;
+        }
+    }
+}
---
+++ src/main/java/factory/simple/Sender.java
@@ -0,0 +1,8 @@
+package factory.simple;
+
+/**
+ * Created by An0nymous on 15/8/27.
+ */
+public interface Sender {
+    public void Send();
+}
